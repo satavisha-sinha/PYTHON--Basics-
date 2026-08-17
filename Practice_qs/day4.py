f3dcbcf2bbc8c92c1543cd639e23263750f28876
satavish-sinha <satavishasinha123@gmail.com>
2026-08-17 23:24:38 +0530

    new practice qs

--- Practice_qs/day4.py
+++ Practice_qs/day4.py
@@ -20,9 +20,17 @@
 # 3.Write a code to detect doouble space in a string.
 
 # st="This is a  string "
+
 # print(st.find("  "))
 
 # # 4. Write a code to replace the double space in string in problem 3.
 
 # print(st.replace("  "," ")) string are immutable the changes do not occur in original str.
 
+# Write a program to format the following letter using escape sequence.
+
+letter="Dear Satavisha, Congratulation !, you are selected for this course."
+print(letter)
+
+new_letter="Dear Satavisha,\nCongratulation !,\n\tYou are selected for this course."
+print(new_letter)
