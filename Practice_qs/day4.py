4b516e3c4c1896f4542d4f255ef1003293e66c3f
satavish-sinha <satavishasinha123@gmail.com>
2026-08-18 23:13:30 +0530

    changes

--- Practice_qs/day4.py
+++ Practice_qs/day4.py
@@ -1,31 +1,31 @@
 # 1. Write the name given by the user and shhow that in a text message. 
 
-# name=input("Enter your name : ")
-# age=int(input("Enter your age : "))
-# cname=input("Enter your course name : ")
+name=input("Enter your name : ")
+age=int(input("Enter your age : "))
+cname=input("Enter your course name : ")
 
-# print(f"My name is {name} and I am a B.Tech student.")
+print(f"My name is {name} and I am a B.Tech student.")
 
-# print(f'''---------My CV--------
+print(f'''---------My CV--------
       
-# NAME : {name}
-# AGE  : {age}
-# COURSE : {cname}''')
+NAME : {name}
+AGE  : {age}
+COURSE : {cname}''')
 
 # 2. Write a code to change the words in a given string.
 
-# letter=" I want to meet //you// at //sunday// //afternoon//"
-# print(letter.replace("//you//","Priya").replace("//sunday//","friday").replace("//afternoon//","night"))
+letter=" I want to meet //you// at //sunday// //afternoon//"
+print(letter.replace("//you//","Priya").replace("//sunday//","friday").replace("//afternoon//","night"))
 
 # 3.Write a code to detect doouble space in a string.
 
-# st="This is a  string "
+st="This is a  string "
 
-# print(st.find("  "))
+print(st.find("  "))
 
 # # 4. Write a code to replace the double space in string in problem 3.
 
-# print(st.replace("  "," ")) string are immutable the changes do not occur in original str.
+print(st.replace("  "," ")) #string are immutable the changes do not occur in original str.
 
 # Write a program to format the following letter using escape sequence.
 
